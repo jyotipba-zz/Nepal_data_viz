3800a090d06cc7b77075abe0b769160ce0a11c86
Jyoti <jyoti.bartaula@integrify.io>
2019-11-26 17:22:07 +0200

    draw choropleth map showing female with bank account by district

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,9 +1,8 @@
 from myapp import app
 from flask import render_template
-from myapp.utils import make_map
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    make_map()
-    return render_template('index.html', title = 'Nepal visualization', text = 'Nepal map')
+    return render_template('index.html', title = 'Nepal visualization', text = 'Nepal')
