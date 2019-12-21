bd628199096f440250a1b4da295095181ecafb71
Jyoti <jyoti.bartaula@integrify.io>
2019-12-21 18:24:46 +0200

    emed bokeh app in flask app

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,8 +1,17 @@
 from myapp import app
-from flask import render_template
+from flask import render_template,url_for
+from bokeh.embed import server_document
 
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return render_template('index.html', title = 'Nepal visualization', text = 'Nepal')
+def home():
+    return render_template('index.html', title = 'Nepal visualization')
+
+
+@app.route('/visualization')
+def visual():
+
+	script = server_document(url='http://localhost:5006/run')
+	print(script)
+	return render_template('visual.html', script=script)
